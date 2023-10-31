86d8e8f96dd5634828931df3b0dcd0f3b4623ac2
Hui Sung <59020441+iamdeveloperz@users.noreply.github.com>
2023-10-31 10:43:38 +0900

    [level 1] Title: 약수의 합, Time: 0.01 ms, Memory: 4.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/12928. 약수의 합/약수의 합.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(int n) {
+    int answer = 0;
+    
+    int i;
+
+    for(i = 1; i * i < n; ++i){
+        if(n % i == 0){
+            answer += i;
+            answer += n/i;
+        }
+    }
+
+    if(i * i == n)
+        answer += i;
+
+    
+    return answer;
+}
